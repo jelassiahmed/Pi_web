ebf3ac8118933adbf6425579d20e9d49becce084
jelassiahmed <ahmed.jelassi@esprit.tn>
2022-05-10 22:11:51 +0100

    make it better

--- Frontend/src/views/admin/Dashboard.js
+++ Frontend/src/views/admin/Dashboard.js
@@ -19,9 +19,6 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="flex flex-wrap mt-4">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardPageVisits />
-        </div>
         <div className="w-full xl:w-4/12 px-4">
           <CardSocialTraffic />
         </div>
--- Frontend/src/views/admin/Settings.js
+++ Frontend/src/views/admin/Settings.js
@@ -4,6 +4,7 @@ import React from "react";
 
 import CardSettings from "components/Cards/CardSettings.js";
 import CardProfile from "components/Cards/CardProfile.js";
+import CardPageVisits from "../../components/Cards/CardPageVisits";
 
 export default function Settings() {
   return (
@@ -14,6 +15,9 @@ export default function Settings() {
         </div>
         <div className="w-full lg:w-4/12 px-4">
           <CardProfile />
+        </div>
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardPageVisits />
         </div>
       </div>
     </>
