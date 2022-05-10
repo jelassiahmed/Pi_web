2ed8a8c8115d257f63248872e146335155ebf16e
jelassiahmed <ahmed.jelassi@esprit.tn>
2022-05-10 23:30:35 +0100

    make it better

--- Frontend/src/api/Api.js
+++ Frontend/src/api/Api.js
@@ -4,14 +4,14 @@ const url = 'http://localhost:5000';
 
 // Complaint API //
 export const getAllComplaint = () => axios.get(`/complaint/getComplaints`);
-export const createComplaint = (newComplaint)=> axios.post(`${url}/complaint/addComplaint`, newComplaint);
-export const updateComplaint = (id, updateComplaint) => axios.patch(`${url}/complaint/updateComplaint/${id}`, updateComplaint);
-export const deleteComplaint = (id) => axios.delete(`${url}/complaint/deleteComplaint/${id}`);
-export const getComplaintByUserId = (id) => axios.get(`${url}/complaint/getComplaintByUserId/${id}`);
-export const getComplaintByStatus = (status) => axios.get(`${url}/complaint/getComplaintByStatus/${status}`);
-export const getComplaintByUserIdAndStatus = (id,status) => axios.get(`${url}/complaint/getComplaintByStatus/${id}/${status}`);
-export const updateComplaintStatus = (id,etat) => axios.patch(`${url}/complaint/update_status/${id}`,etat);
-export const sendMessage = (id,msg) => axios.patch(`${url}/complaint/send_msg/${id}`,msg);
+export const createComplaint = (newComplaint)=> axios.post(`/complaint/addComplaint`, newComplaint);
+export const updateComplaint = (id, updateComplaint) => axios.patch(`/complaint/updateComplaint/${id}`, updateComplaint);
+export const deleteComplaint = (id) => axios.delete(`/complaint/deleteComplaint/${id}`);
+export const getComplaintByUserId = (id) => axios.get(`/complaint/getComplaintByUserId/${id}`);
+export const getComplaintByStatus = (status) => axios.get(`/complaint/getComplaintByStatus/${status}`);
+export const getComplaintByUserIdAndStatus = (id,status) => axios.get(`/complaint/getComplaintByStatus/${id}/${status}`);
+export const updateComplaintStatus = (id,etat) => axios.patch(`/complaint/update_status/${id}`,etat);
+export const sendMessage = (id,msg) => axios.patch(`/complaint/send_msg/${id}`,msg);
 
 // export const fetchProductById = (id)=> axios.get(`${url}/products/${id}`);
 // export const smartFetchProduct = (pagination)=> axios.post(`${url}/products/page`, pagination);
